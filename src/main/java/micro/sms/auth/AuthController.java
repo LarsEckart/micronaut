9a968f521e5aab990944eb47437e1bbf0c41109a
Lars Eckart <lars.eckart@gmail.com>
2019-07-10 15:35:45 +0300

    return unauthorized when empty header

--- src/main/java/micro/sms/auth/AuthController.java
+++ src/main/java/micro/sms/auth/AuthController.java
@@ -1,18 +1,19 @@
 package micro.sms.auth;
 
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Header;
 
 @Controller("/sms/auth")
 public class AuthController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
-    public HttpResponse index() {
-        return HttpResponse.status(HttpStatus.BAD_REQUEST);//.body("success");
+    public HttpResponse index(@Header("sender") String sender) {
+        if (sender == null || sender.isBlank()) {
+            return HttpResponse.unauthorized();
+        }
+        return HttpResponse.badRequest();//.body("success");
     }
-
-
 }
--- src/test/java/micro/sms/auth/AuthTests.java
+++ src/test/java/micro/sms/auth/AuthTests.java
@@ -15,13 +15,24 @@ class AuthTests {
     EmbeddedServer server;
 
     @Test
-    void returns_401_when_no_header_sender() {
+    void returns_400_when_no_header_sender() {
         given().
             port(server.getPort()).
         when().
             get("/sms/auth").
         then().
             statusCode(400);
+    }
+
+    @Test
+    void returns_401_when_header_sender_empty() {
+        given().
+            port(server.getPort()).
+            header("sender", "").
+        when().
+            get("/sms/auth").
+        then().
+            statusCode(401);
     }
 
 }
