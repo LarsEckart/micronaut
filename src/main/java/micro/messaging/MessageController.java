e5449de103d67ccabe2e187aa68e41f5fce740fc
Lars Eckart <lars.eckart@gmail.com>
2019-07-11 14:17:36 +0300

    validate post request body

--- src/main/java/micro/messaging/MessageController.java
+++ src/main/java/micro/messaging/MessageController.java
@@ -1,10 +1,14 @@
 package micro.messaging;
 
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Controller("/messaging/send")
 public class MessageController {
@@ -16,7 +20,7 @@ public class MessageController {
     }
 
     @Post(produces = MediaType.TEXT_PLAIN)
-    public HttpResponse index(@Header("auth") String auth) {
+    public HttpResponse index(@Header("auth") String auth, HttpRequest<Map<String, Object>> request) {
         if (auth == null || auth.isBlank()) {
             return HttpResponse.unauthorized();
         }
@@ -26,6 +30,25 @@ public class MessageController {
             return HttpResponse.unauthorized();
         }
         if (repository.validToken(split[0], split[1])) {
+
+            Optional<Map<String, Object>> body = request.getBody();
+            String text = (String) body.get().get("text");
+            if (text == null) {
+                return HttpResponse.badRequest("text is mandatory");
+            }
+            String from = (String) body.get().get("from");
+            if (from == null) {
+                return HttpResponse.badRequest("from is mandatory");
+            }
+            String to = (String) body.get().get("to");
+            if (to == null) {
+                return HttpResponse.badRequest("to is mandatory");
+            }
+            String displayName = (String) body.get().get("display_name");
+            if (displayName == null) {
+                return HttpResponse.badRequest("display_name is mandatory");
+            }
+
             return HttpResponse.ok();
         }
         return HttpResponse.unauthorized();
--- src/test/java/micro/messaging/SendMessageTests.java
+++ src/test/java/micro/messaging/SendMessageTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @MicronautTest
 class SendMessageTests {
@@ -59,7 +60,7 @@ class SendMessageTests {
     }
 
     @Test
-    void ok_when_auth_header_present_and_valid_token() {
+    void ok_when_auth_header_present_and_valid_token_and_body() {
         Response sender = given().
                     port(server.getPort()).
                     header("sender", "123").
@@ -77,11 +78,119 @@ class SendMessageTests {
         given().
             port(server.getPort()).
             contentType(ContentType.JSON).
-            header("auth", "123_"+sender.getBody().print()).
+            header("auth", "123_" + sender.getBody().print()).
             body(payload).
         when().
             post("/messaging/send").
         then().
             statusCode(200);
+    }
+
+    @Test
+    void response_code_400_when_no_text() {
+        Response sender = given().
+                    port(server.getPort()).
+                    header("sender", "123").
+                when().
+                    get("/messaging/auth");
+
+
+        String payload = "{\n"
+                + "  \"to\": \"01603668822\",\n"
+                + "  \"from\": \"0211\",\n"
+                + "  \"display_name\": \"ECHO\"\n"
+                + "}";
+
+        given().
+            port(server.getPort()).
+            contentType(ContentType.JSON).
+            header("auth", "123_" + sender.getBody().print()).
+            body(payload).
+        when().
+            post("/messaging/send").
+        then().
+            statusCode(400).
+            body(equalTo("text is mandatory"));
+    }
+
+    @Test
+    void response_code_400_when_no_to() {
+        Response sender = given().
+                    port(server.getPort()).
+                    header("sender", "123").
+                when().
+                    get("/messaging/auth");
+
+
+        String payload = "{\n"
+                + "  \"text\": \"hello world\",\n"
+                + "  \"from\": \"0211\",\n"
+                + "  \"display_name\": \"ECHO\"\n"
+                + "}";
+
+        given().
+            port(server.getPort()).
+            contentType(ContentType.JSON).
+            header("auth", "123_" + sender.getBody().print()).
+            body(payload).
+        when().
+            post("/messaging/send").
+        then().
+            statusCode(400).
+            body(equalTo("to is mandatory"));
+    }
+
+    @Test
+    void response_code_400_when_no_from() {
+        Response sender = given().
+                    port(server.getPort()).
+                    header("sender", "123").
+                when().
+                    get("/messaging/auth");
+
+
+        String payload = "{\n"
+                + "  \"text\": \"hello world\",\n"
+                + "  \"to\": \"01603668822\",\n"
+                + "  \"display_name\": \"ECHO\"\n"
+                + "}";
+
+        given().
+            port(server.getPort()).
+            contentType(ContentType.JSON).
+            header("auth", "123_" + sender.getBody().print()).
+            body(payload).
+        when().
+            post("/messaging/send").
+        then().
+            statusCode(400).
+            body(equalTo("from is mandatory"));
+    }
+
+    @Test
+    void response_code_400_when_no_display_name() {
+        Response sender = given().
+                    port(server.getPort()).
+                    header("sender", "123").
+                when().
+                    get("/messaging/auth");
+
+
+        String payload = "{\n"
+                + "  \"text\": \"hello world\",\n"
+                + "  \"to\": \"01603668822\",\n"
+                + "  \"from\": \"0211\"\n"
+                + "}";
+
+        given().
+            port(server.getPort()).
+            contentType(ContentType.JSON).
+            header("auth", "123_" + sender.getBody().print()).
+            body(payload).
+        when().
+            post("/messaging/send").
+        then().
+            statusCode(400).
+            body(equalTo("display_name is mandatory"));
     }
 }
