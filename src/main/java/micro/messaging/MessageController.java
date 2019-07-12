258d22913c58315bdec943546e40de5c738e7c22
Lars Eckart <lars.eckart@gmail.com>
2019-07-12 13:11:58 +0300

    String.valueOf(null) returns "null" !

--- src/main/java/micro/messaging/MessageController.java
+++ src/main/java/micro/messaging/MessageController.java
@@ -1,19 +1,21 @@
 package micro.messaging;
 
+import com.google.common.flogger.FluentLogger;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.logging.Level;
 
-@Slf4j
 @Controller("/messaging/send")
 public class MessageController {
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
     private TokenRepository repository;
 
@@ -27,8 +29,8 @@ public class MessageController {
             return HttpResponse.unauthorized();
         }
 
-        log.info("auth: " + auth);
-        log.info("body: " + request.getBody().get().toString());
+        logger.at(Level.INFO).log("auth: " + auth);
+        logger.at(Level.INFO).log("body: " + request.getBody().get().toString());
 
         String[] split = auth.split("_");
         if (split.length != 2) {
@@ -37,20 +39,21 @@ public class MessageController {
         if (repository.validToken(split[0], split[1])) {
 
             Optional<Map<String, Object>> body = request.getBody();
-            String text = (String) body.get().get("text");
-            if (text == null) {
+
+            String text = String.valueOf(body.get().get("text"));
+            if (text == null || "null".equals(text)) {
                 return HttpResponse.badRequest("text is mandatory");
             }
             String from = String.valueOf(body.get().get("from"));
-            if (from == null) {
+            if (from == null|| "null".equals(from)) {
                 return HttpResponse.badRequest("from is mandatory");
             }
             String to = String.valueOf(body.get().get("to"));
-            if (to == null) {
+            if (to == null|| "null".equals(to)) {
                 return HttpResponse.badRequest("to is mandatory");
             }
             String displayName = String.valueOf(body.get().get("display_name"));
-            if (displayName == null) {
+            if (displayName == null || "null".equals(displayName)) {
                 return HttpResponse.badRequest("display_name is mandatory");
             }
 
