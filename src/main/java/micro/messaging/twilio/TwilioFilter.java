2b7573142a9df799ef215a126a06cf3db0235246
Lars Eckart <lars.eckart@gmail.com>
2019-07-13 23:08:07 +0300

    auth required ;)

---
+++ src/main/java/micro/messaging/twilio/TwilioFilter.java
@@ -0,0 +1,26 @@
+package micro.messaging.twilio;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MutableHttpRequest;
+import io.micronaut.http.annotation.Filter;
+import io.micronaut.http.filter.ClientFilterChain;
+import io.micronaut.http.filter.HttpClientFilter;
+import org.reactivestreams.Publisher;
+
+@Filter("/**")
+@Requires(property = Config.PREFIX + ".accountSid")
+@Requires(property = Config.PREFIX + ".authToken")
+public class TwilioFilter  implements HttpClientFilter {
+
+    private final Config configuration;
+
+    public TwilioFilter(Config configuration ) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public Publisher<? extends HttpResponse<?>> doFilter(MutableHttpRequest<?> request, ClientFilterChain chain) {
+        return chain.proceed(request.basicAuth(configuration.accountSid, configuration.authToken));
+    }
+}
