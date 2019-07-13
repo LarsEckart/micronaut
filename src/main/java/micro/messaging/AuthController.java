ffe1137584e2e976b46f10233b695e8d818f115b
Lars Eckart <lars.eckart@gmail.com>
2019-07-13 17:36:06 +0300

    test env variables

--- src/main/java/micro/messaging/AuthController.java
+++ src/main/java/micro/messaging/AuthController.java
@@ -1,5 +1,6 @@
 package micro.messaging;
 
+import io.micronaut.context.annotation.Value;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
@@ -9,7 +10,12 @@ import io.micronaut.http.annotation.Header;
 @Controller("/messaging/auth")
 public class AuthController {
 
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AuthController.class);
+
     private TokenRepository repository;
+
+    @Value("${myapp.endpoint}")
+    protected String endpoint;
 
     public AuthController(TokenRepository repository) {
         this.repository = repository;
@@ -17,6 +23,7 @@ public class AuthController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public HttpResponse index(@Header("sender") String sender) {
+        log.info(endpoint);
         if (sender == null || sender.isBlank()) {
             return HttpResponse.badRequest();
         }
