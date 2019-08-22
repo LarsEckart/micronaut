c462f9e31a0d5bf8844bde3cbf5cceab7b3cbe3d
Lars Eckart <lars.eckart@gmail.com>
2019-08-22 07:23:12 +0300

    public final fields for DTO

--- src/main/java/micro/messaging/twilio/ApiResponse.java
+++ src/main/java/micro/messaging/twilio/ApiResponse.java
@@ -2,84 +2,25 @@ package micro.messaging.twilio;
 
 class ApiResponse {
 
-    private String sid;
-    private String date_created;
-    private String date_updated;
-    private String to;
-    private String from;
-    private String body;
-    private String error_code;
-    private String error_message;
+    public final String sid;
+    public final String date_created;
+    public final String date_updated;
+    public final String to;
+    public final String from;
+    public final String body;
+    public final String error_code;
+    public final String error_message;
 
-    public String getSid() {
-        return sid;
-    }
-
-    public ApiResponse setSid(String sid) {
+    public ApiResponse(
+            String sid, String date_created, String date_updated, String to, String from, String body, String error_code,
+            String error_message) {
         this.sid = sid;
-        return this;
-    }
-
-    public String getDate_created() {
-        return date_created;
-    }
-
-    public ApiResponse setDate_created(String date_created) {
         this.date_created = date_created;
-        return this;
-    }
-
-    public String getDate_updated() {
-        return date_updated;
-    }
-
-    public ApiResponse setDate_updated(String date_updated) {
         this.date_updated = date_updated;
-        return this;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public ApiResponse setTo(String to) {
         this.to = to;
-        return this;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public ApiResponse setFrom(String from) {
         this.from = from;
-        return this;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public ApiResponse setBody(String body) {
         this.body = body;
-        return this;
-    }
-
-    public String getError_code() {
-        return error_code;
-    }
-
-    public ApiResponse setError_code(String error_code) {
         this.error_code = error_code;
-        return this;
-    }
-
-    public String getError_message() {
-        return error_message;
-    }
-
-    public ApiResponse setError_message(String error_message) {
         this.error_message = error_message;
-        return this;
     }
 }
