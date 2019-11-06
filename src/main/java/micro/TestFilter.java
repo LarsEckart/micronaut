f9c63a223c10a63259a75f98776e5686aa0a96d4
Lars Eckart <lars.eckart@gmail.com>
2019-11-06 11:23:43 +0200

    bump to micronaut 1.2.6

---
+++ src/main/java/micro/TestFilter.java
@@ -0,0 +1,17 @@
+package micro;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Filter;
+import io.micronaut.http.filter.FilterChain;
+import io.micronaut.http.filter.HttpFilter;
+import org.reactivestreams.Publisher;
+
+@Filter("/**")
+public class TestFilter implements HttpFilter {
+
+    @Override
+    public Publisher<? extends HttpResponse<?>> doFilter(HttpRequest<?> request, FilterChain chain) {
+        return chain.proceed(request);
+    }
+}
