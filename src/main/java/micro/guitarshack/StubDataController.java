a1c68fc26b6bfea5a1cabac25cd4d99c28133642
Lars Eckart <lars.eckart@hey.com>
2021-06-17 23:47:26 +0300

    randomized data

--- src/main/java/micro/guitarshack/StubDataController.java
+++ src/main/java/micro/guitarshack/StubDataController.java
@@ -6,23 +6,25 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
+import java.util.Random;
 import org.slf4j.Logger;
 
 @Controller
 class StubDataController {
 
   private static final Logger log = getLogger(StubDataController.class);
+  private final Random random = new Random();
 
   @Get("/default/product")
   public HttpResponse<Product> product(@QueryValue String id) {
-    log.info("Received request to /");
-    return HttpResponse.ok(new Product(Integer.valueOf(id), 2, 3));
+    log.info("Received request to /product");
+    return HttpResponse.ok(new Product(Integer.parseInt(id), random.nextInt(10), random.nextInt(45)));
   }
 
   @Get("/default/sales")
   public HttpResponse<SalesTotal> salesTotal(@QueryValue String id) {
-    log.info("Received request to /");
-    return HttpResponse.ok(new SalesTotal(1));
+    log.info("Received request to /sales");
+    return HttpResponse.ok(new SalesTotal(random.nextInt(20)));
   }
 
 }
