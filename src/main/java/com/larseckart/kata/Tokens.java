6cefd2d76e1bc2e41f31857b70994afa8d311231
Lars Eckart <lars.eckart@hey.com>
2024-02-22 13:39:43 +0200

    endpoint for token service kata

---
+++ src/main/java/com/larseckart/kata/Tokens.java
@@ -0,0 +1,33 @@
+package com.larseckart.kata;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller("/tokens")
+class Tokens {
+
+  @Get("/create")
+  public Response createToken() {
+    ZonedDateTime now = ZonedDateTime.now();
+    Duration aliveUntil = Duration.ofMinutes(2);
+    ZonedDateTime expires = now.plus(aliveUntil);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss z");
+    String start = formatter.format(now);
+    String end = formatter.format(expires);
+
+    return new Response("abc", "bear", aliveUntil.getSeconds(), "you", start, end);
+  }
+
+  record Response(
+      String access_token,
+      String token_type,
+      Long expires_in,
+      String requested_by,
+      String issued,
+      String expires) {
+  }
+
+}
