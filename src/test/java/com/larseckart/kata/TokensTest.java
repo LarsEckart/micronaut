c8ab6e436ba6c765f2c2c0480096f3deb78e3be4
Lars Eckart <lars.eckart@hey.com>
2024-02-23 13:05:18 +0200

    rename test

--- src/test/java/com/larseckart/kata/TokensTest.java
+++ src/test/java/com/larseckart/kata/TokensTest.java
@@ -18,22 +18,23 @@ class TokensTest {
   EmbeddedServer server;
 
   @Test
-  void exposes_health_endpoint_due_to_micronaut_management_dependency() {
-    Response response = given()
-        .port(server.getPort())
-        .body("grant_type=password&username=test&password=1234")
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .when()
-        .post("/tokens/create")
-        .then()
-        .extract()
-        .response();
+  void test_token_retrieval() {
+    Response response =
+        given()
+            .port(server.getPort())
+            .body("grant_type=password&username=test&password=1234")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .when()
+            .post("/tokens/create")
+            .then()
+            .extract()
+            .response();
 
     String print = response.getBody().print();
-    String json = print.replaceAll("abc\\d\\d\\d\\d", "[TOKEN]");
+    String json = print.replaceAll("abc\\d\\d\\d\\d?", "[TOKEN]");
     JsonJacksonApprovals.verifyAsJson(json, new Options(new RegExScrubber(
         "\\b[A-Z][a-z]{2},\\s\\d{2}\\s[A-Z][a-z]{2}\\s\\d{4}\\s\\d{2}:\\d{2}:\\d{2}\\s[A-Z]{3}\\b",
-        "date")));
+        "[DATE]")));
   }
 
 }
